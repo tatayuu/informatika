87df7908fe352dcdb60bcc470185ca9736de50d5
tatayuu <145945967+tatayuu@users.noreply.github.com>
2024-08-31 12:43:57 +0700

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,17 @@
+#penulisan statement
+print("Hello World")
+
+#penulisan string
+Judul = "Pergi ke Pantai"
+print(Judul)
+
+#penulisan case
+method_name = "Fita Khoirun Nisa'"
+print(method_name)
+
+#penulisan blok program
+a = 25
+b = 45
+if b > a:
+    print("b is greater than a")
+    
